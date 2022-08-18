82923474a6693264261666c915bf4d0266d8e6b2
gjsgabriel <gjsgabriel83@gmail.com>
2022-08-18 14:20:38 -0300

    Consumer com expressão lambda declarada

--- src/application/ProgramConsumer.java
+++ src/application/ProgramConsumer.java
@@ -3,9 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
-
-import util.PriceUpdate;
+import java.util.function.Consumer;
 
 public class ProgramConsumer {
 
@@ -18,7 +16,9 @@ public class ProgramConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 		
